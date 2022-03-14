5512587e52573feed10b72a2ec029874228a2966
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-03-14 12:40:45 -0700

    Currently trying to add tests cases, import is current issue

--- Tests/test_Tokenizer.py
+++ Tests/test_Tokenizer.py
@@ -1,6 +1,13 @@
 import unittest
+import src.Tokenizer as Tokenizer
+ 
 
-import Tokenizer
+
+class TestTokens(unittest.TestCase):
+    def test_run(self):
+        test_input = '9+9'
+        expected_text = '[INT:9,PLUS,INT:9]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
 
 if __name__  == '__main__':
     unittest.main()
