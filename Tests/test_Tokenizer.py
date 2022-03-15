9ea50dcb0b919a5f60b2cfd2fe597eff1742b2f3
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-03-14 21:43:59 -0700

    test file used in src for now

--- Tests/test_Tokenizer.py
+++ Tests/test_Tokenizer.py
@@ -1,17 +1,28 @@
-import unittest
-import src.Tokenizer as Tokenizer
- 
+####################################################################################
+####################################################################################
+############ Using test file in the src folder. Igonore for now ####################
+####################################################################################
+####################################################################################
 
 
-class TestTokens(unittest.TestCase):
-    def test_run(self):
-        test_input = '9+9'
-        expected_text = '[INT:9,PLUS,INT:9]'
-        self.assertEqual(Tokenizer.run(test_input), expected_text)
+# from importlib import import_module
+# import unittest
+# import sys
 
-if __name__  == '__main__':
-    unittest.main()
+# sys.path.append(1, '../')
+# from source.Tokenizer import Tokenizer as module
 
+
+
+# # NOT 100% SURE THIS TEST CASE WILL WORK TRYING TO GET IMPORT TO WORK FOR NOW
+# class TestTokens(unittest.TestCase):
+#     def test_additions(self):
+#         test_input = '9+9'
+#         expected_text = '[INT:9,PLUS,INT:9]'
+#         self.assertEqual(module.run(test_input), expected_text)
+
+# if __name__  == '__main__':
+#     unittest.main()
 
 
     
