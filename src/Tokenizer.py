14748663bfc44131ae234e62a654bff34cc149e8
adrherr <adrian,herrera1447@gmail.com>
2022-03-14 21:00:11 -0700

    Integers and Floats display properly as tokens

--- src/Tokenizer.py
+++ src/Tokenizer.py
@@ -1,29 +1,34 @@
 #CONSTANTS#
-DIGITS = '01234567890'
+DIGITS = '0123456789'
 
 #TOKENS#
 TT_INT      = 'INT'
 TT_FLOAT    = 'FLOAT'
 TT_STRING   = 'STRING'
+
 TT_VARIABLE = 'VARIABLE'
+
 TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
 TT_MULTIPLY = 'MULTIPLY'
 TT_DIVIDE   = 'DIVIDE'
+
 TT_LPARAN   = 'LPARAN'
 TT_RPARAN   = 'RPARAN'
 TT_LCURLY   = 'LCURLY'
 TT_RCURLY   = 'RCURLY'
+
+TT_DEF      = 'FUNCTION'
 TT_WHILE    = 'WHILE'
 TT_IF       = 'IF'
 
 #TOKEN CLASS#
 class Token:
-    def __init__(self, type_, value = None):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
@@ -41,12 +46,12 @@ class Lexer:
 
     def make_tokens(self):
         tokens = []
+
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.advance()
             elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
-                self.advance()
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
@@ -71,12 +76,6 @@ class Lexer:
             elif self.current_char == '}':
                 tokens.append(Token(TT_RCURLY))
                 self.advance()
-            elif self.current_char == 'if':
-                tokens.append(Token(TT_IF))
-                self.advance()
-            elif self.current_char == 'while':
-                tokens.append(Token(TT_WHILE))
-                self.advance()
             else:
                 char = self.current_char
                 self.advance()
@@ -97,10 +96,10 @@ class Lexer:
                 num_str += self.current_char
             self.advance()
 
-            if dot_count == 0:
-                return Token(TT_INT, int(num_str))
-            else:
-                return Token(TT_FLOAT, float(num_str))
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
 
 #ERROR CLASS#
 class Error:
