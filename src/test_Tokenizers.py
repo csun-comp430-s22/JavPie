55b7bfc060dffb7b3153b440853dde399fd5eaa4
Sarai2028 <saraicantype@gmail.com>
2022-03-14 21:29:37 -0700

    -Coverage Report/Added Tests

--- src/test_Tokenizers.py
+++ src/test_Tokenizers.py
@@ -7,6 +7,47 @@ class TestTokens(unittest.TestCase):
         test_input = '1+1'
         expected_text = '[INT:1, PLUS, INT:1]'
         self.assertEqual(Tokenizer.run(test_input), expected_text)
+class TestTokens2(unittest.TestCase):
+    def test_additions(self):
+        test_input = '(3)'
+        expected_text = '[LPAREN, INT:3, RPAREN]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+
+
+class TestTokens3(unittest.TestCase):
+    def test_additions(self):
+        test_input = '{5}'
+        expected_text = '[LCURLY, INT:3, RCURLY]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+
+class TestTokens4(unittest.TestCase):
+    def test_additions(self):
+        test_input = '10/2'
+        expected_text = '[INT: 10, DIVIDE, INT: 2]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+
+class TestTokens5(unittest.TestCase):
+    def test_additions(self):
+        test_input = '10 * 2'
+        expected_text = '[INT:10, MULTIPLY, INT:2]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+
+class TestTokens6(unittest.TestCase):
+    def test_additions(self):
+        test_input = '+-'
+        expected_text = '[PLUS, MINUS]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+class TestTokens7(unittest.TestCase):
+    def test_additions(self):
+        test_input = '2.5'
+        expected_text = '[FLOAT]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
+
+class TestTokens_Error(unittest.TestCase):
+    def test_additions(self):
+        test_input = 'string'
+        expected_text = '[ERROR]'
+        self.assertEqual(Tokenizer.run(test_input), expected_text)
 
 if __name__  == '__main__':
     unittest.main()
