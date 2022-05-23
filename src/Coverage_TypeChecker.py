1500041ad0c39b7e6286066de777f75c686a3364
MauricioCasti <71904932+MauricioCasti@users.noreply.github.com>
2022-05-22 21:21:51 -0700

    added type checker test file coverage version

---
+++ src/Coverage_TypeChecker.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+
+sys.path.insert(0,'src')
+##from Tokenizer import run
+from Tokenizer import *
+
+
+##
+## Type Checking, or in python, Type Hinting,
+## can only be run in python, and our language being created
+## will not be able to support it.
+##
+
+##
+## And without making this code compatible
+## or witout the use for external tools
+## which are not included in this project,
+## there will be no tests regarding type hinting.
+##
+
+##
+## This will also be explained in the presentation video
+## and in documentation 
+##
+
+if __name__  == '__main__':
+    unittest.main()
